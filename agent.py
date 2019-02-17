fd751a6ea69f19eb03dd454f104965924e3fd8a4
Muh Alif Ahsanul Islam <alifahsanul@gmail.com>
2019-02-17 16:19:20 +0900

    Make code workable in my local env

--- agent.py
+++ agent.py
@@ -1,79 +1,84 @@
 import copy
 import os
-from collections import namedtuple
+from collections import namedtuple, deque
 import torch.optim as optim
 import random
+import numpy as np
 from model import Actor, Critic
 import torch
-import numpy as np
+from torch.nn import functional as F
 
 
-class Agent():        
-    def __init__(self, 
-        device,
-        state_size, n_agents, action_size, random_seed,
-        buffer_size, batch_size, gamma, TAU, lr_actor, lr_critic, weight_decay,
-        checkpoint_folder = './'):        
+BUFFER_SIZE = int(1e5)
+BATCH_SIZE = 128
+GAMMA = 0.99
+TAU = 1e-3
+LR_ACTOR = 1e-4
+LR_CRITIC = 1e-4
+WEIGHT_DECAY = 0
+SEED = 42
 
-        self.DEVICE = device
-
+class Agent():
+    def __init__(self,
+                device,
+                state_size,
+                n_agents,
+                action_size,
+                checkpoint_folder = './'):
+        self.device = device
         self.state_size = state_size
         self.n_agents = n_agents
         self.action_size = action_size
-        self.seed = random.seed(random_seed)
+        self.seed = random.seed(SEED)
 
-        # Hyperparameters
-        self.BUFFER_SIZE = buffer_size
-        self.BATCH_SIZE = batch_size
-        self.GAMMA = gamma
-        self.TAU = TAU
-        self.LR_ACTOR = lr_actor
-        self.LR_CRITIC = lr_critic
-        self.WEIGHT_DECAY = weight_decay
+        self.buffer_size = BUFFER_SIZE
+        self.batch_size = BATCH_SIZE
+        self.gamma = GAMMA
+        self.tau = TAU
+        self.lr_actor = LR_ACTOR
+        self.lr_critic = LR_CRITIC
+        self.weight_decay = WEIGHT_DECAY
+        self.checkpoint_folder = checkpoint_folder
 
-        self.CHECKPOINT_FOLDER = checkpoint_folder
+        self.actor_local = Actor(state_size, action_size, SEED).to(self.device)
+        self.actor_target = Actor(state_size, action_size, SEED).to(self.device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
-        # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed).to(self.DEVICE)
-        self.actor_target = Actor(state_size, action_size, random_seed).to(self.DEVICE)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=self.LR_ACTOR)
+        self.critic_local = Critic(state_size, action_size, SEED).to(self.device)
+        self.critic_target = Critic(state_size, action_size, SEED).to(self.device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
-        # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed).to(self.DEVICE)
-        self.critic_target = Critic(state_size, action_size, random_seed).to(self.DEVICE)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=self.LR_CRITIC, weight_decay=self.WEIGHT_DECAY)
+        if os.path.isfile(self.checkpoint_folder + 'checkpoint_actor.pth') and os.path.isfile(self.checkpoint_folder + 'checkpoint_critic.pth'):
+            self.actor_local.load_state_dict(torch.load(self.checkpoint_folder + 'checkpoint_actor.pth'))
+            self.actor_target.load_state_dict(torch.load(self.checkpoint_folder + 'checkpoint_actor.pth'))
 
-        if os.path.isfile(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth') and os.path.isfile(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'):
-            self.actor_local.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth'))
-            self.actor_target.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth'))
+            self.critic_local.load_state_dict(torch.load(self.checkpoint_folder + 'checkpoint_critic.pth'))
+            self.critic_target.load_state_dict(torch.load(self.checkpoint_folder + 'checkpoint_critic.pth'))
 
-            self.critic_local.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'))
-            self.critic_target.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'))
-
-        # Noise process
-        self.noise = OUNoise((n_agents, action_size), random_seed)
+        # Noise
+        self.noise = OUNoise((n_agents, action_size), SEED)
 
         # Replay memory
-        self.memory = ReplayBuffer(device, action_size, self.BUFFER_SIZE, self.BATCH_SIZE, random_seed)
-    
+        self.memory = ReplayBuffer(device, action_size, BUFFER_SIZE, BATCH_SIZE, SEED)
+
     def step(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
         for i in range(self.n_agents):
             self.memory.add(state[i,:], action[i,:], reward[i], next_state[i,:], done[i])
 
-        # Learn, if enough samples are available in memory        
-        if len(self.memory) > self.BATCH_SIZE:
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE:
             experiences = self.memory.sample()
             self.learn(experiences)
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
-        state = torch.from_numpy(state).float().to(self.DEVICE)
+        state = torch.from_numpy(state).float().to(self.device)
         self.actor_local.eval()
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
-        self.actor_local.train()        
+        self.actor_local.train()
         if add_noise:
             action += self.noise.sample()
         return np.clip(action, -1, 1)
@@ -89,7 +94,7 @@ class Agent():
             critic_target(state, action) -> Q-value
         Params
         ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
@@ -99,7 +104,7 @@ class Agent():
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
         # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (self.GAMMA * Q_targets_next * (1 - dones))
+        Q_targets = rewards + (GAMMA * Q_targets_next * (1 - dones))
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
@@ -120,10 +125,10 @@ class Agent():
 
         # ----------------------- update target networks ----------------------- #
         self.soft_update(self.critic_local, self.critic_target)
-        self.soft_update(self.actor_local, self.actor_target)   
-           
+        self.soft_update(self.actor_local, self.actor_target)
+
         self.actor_loss = actor_loss.data
-        self.critic_loss = critic_loss.data        
+        self.critic_loss = critic_loss.data
 
     def soft_update(self, local_model, target_model):
         """Soft update model parameters.
@@ -132,25 +137,25 @@ class Agent():
         ======
             local_model: PyTorch model (weights will be copied from)
             target_model: PyTorch model (weights will be copied to)
-            tau (float): interpolation parameter 
+            tau (float): interpolation parameter
         """
-        tau = self.TAU
+        tau = TAU
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
     def checkpoint(self):
-        torch.save(self.actor_local.state_dict(), self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth')      
-        torch.save(self.critic_local.state_dict(), self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth')  
+        torch.save(self.actor_local.state_dict(), self.checkpoint_folder + 'checkpoint_actor.pth')
+        torch.save(self.critic_local.state_dict(), self.checkpoint_folder + 'checkpoint_critic.pth')
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
     def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
-        self.size = size        
+        self.size = size
         self.mu = mu * np.ones(size)
         self.theta = theta
-        self.sigma = sigma        
+        self.sigma = sigma
         self.seed = random.seed(seed)
         self.reset()
 
@@ -160,7 +165,7 @@ class OUNoise:
 
     def sample(self):
         """Update internal state and return it as a noise sample."""
-        x = self.state        
+        x = self.state
         dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
         self.state = x + dx
         return self.state
@@ -182,12 +187,12 @@ class ReplayBuffer:
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
-    
+
     def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""        
+        """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
-    
+
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
@@ -203,4 +208,3 @@ class ReplayBuffer:
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
-
--- continuous_control.py
+++ continuous_control.py
@@ -14,50 +14,72 @@ import numpy as np
 
 # select this option to load version 2 (with 20 agents) of the environment
 env = UnityEnvironment(file_name='/home/alifahsanul/Documents/RLnanodegree/deep-reinforcement-learning/p2_continuous-control/Reacher_Linux/Reacher.x86_64')
-input('to continue, press enter...')
+#input('to continue, press enter...')
 #%%
 # get the default brain
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
-
-# reset the environment
 env_info = env.reset(train_mode=True)[brain_name]
-
-# number of agents
 num_agents = len(env_info.agents)
-print('Number of agents:', num_agents)
-
-# size of each action
 action_size = brain.vector_action_space_size
-print('Size of each action:', action_size)
-
-# examine the state space 
 states = env_info.vector_observations
 state_size = states.shape[1]
-print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
-print('The state for the first agent looks like:', states[0])
+
+def print_info():
+    print('Number of agents:', num_agents)
+    print('Size of each action:', action_size)
+    print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('The state for the first agent looks like:', states[0])
+print_info()
 
 #%%
-import torch
+#import torch
 from agent import Agent
-reload(agent)
-# hyperparameters
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128        # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-4        # learning rate of the critic
-WEIGHT_DECAY = 0        # L2 weight decay
-DEVICE = 'cpu'# torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-CHECKPOINT_FOLDER = './'
+from collections import deque
+#%%
+folder_path = './'
+device = 'cpu'
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('run on {}'.format(device))
+agent = Agent(device,
+              state_size,
+              num_agents,
+              action_size,
+              folder_path)
 
+#%%
+scores = []
+scores_window = deque(maxlen=100)
+n_episodes = 5
 
-agent = Agent(
-                DEVICE, 
-                state_size, num_agents, action_size, 4, 
-                BUFFER_SIZE, BATCH_SIZE, GAMMA, TAU, LR_ACTOR, LR_CRITIC, WEIGHT_DECAY,
-                CHECKPOINT_FOLDER
-        )
+for episode in range(n_episodes):
+    env_info = env.reset(train_mode=True)[brain_name]
+    states = env_info.vector_observations
+    agent.reset()
+    score = np.zeros(num_agents)
+    while True:
+        actions = agent.act(states)
+        env_info = env.step(actions)[brain_name]
+        next_states = env_info.vector_observations
+        rewards = env_info.rewards
+        dones = env_info.local_done
+        agent.step(states, actions, rewards, next_states, dones)
+        score += rewards
+        states = next_states
+        if np.any(dones):
+            break
+    agent.checkpoint()
+    scores.append(np.mean(score))
+    scores_window.append(np.mean(score))
+    print('\rEpisode: \t{} \tScore: \t{:.2f} \tAverage Score: \t{:.2f}'.format(episode, np.mean(score), np.mean(scores_window)), end="")
+    if np.mean(scores_window) >= 30.0:
+        print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode, np.mean(scores_window)))
+        break
 
-
+#%%
+import matplotlib.pyplot as plt
+# %matplotlib inline
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
